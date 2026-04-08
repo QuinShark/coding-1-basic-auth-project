9d3d564aad18f53307865019e1602264187a7e19
QuinShark <s1002562@student.knoxschools.org>
2026-04-08 14:20:35 +0000

    Added another intit and get_db

--- student_work/application.py
+++ student_work/application.py
@@ -33,6 +33,24 @@ def init_db():
 
 init_db()
 
+def get_db():
+    conn = sqlite3.connect("users.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_db()
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            username TEXT PRIMARY KEY,
+            password TEXT
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+init_db()
+
 # ---------- STYLE ----------
 base_style = """
 <style>
